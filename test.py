4fc785aa10391bba0898703e0e70e7f7778d9bdd
ShayneSL1 <ssluangvisa@gmail.com>
2025-10-31 02:37:25 -0400

    Played with indentation. All the print statements ran fine whether in the if block or not

--- test.py
+++ test.py
@@ -2,3 +2,9 @@ print ('Hello, World!')
 
 
 print ('Hello, Cloud Engineer!')
+if True:
+    print('This is indented')
+    print('this is also in the if block')
+    print('this is a third line that is indented')
+print('This is not idented, so it is outside the if block')
+print('This is a second line that is not indented.')

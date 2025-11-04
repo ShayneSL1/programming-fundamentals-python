3a793258d08b79fa08d4053e7f35a5c8ad7b6d9e
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-03 23:53:28 -0500

    Update test.py
    Working with concatenation using the + operator

--- test.py
+++ test.py
@@ -8,3 +8,12 @@ if True:
     print('this is a third line that is indented')
 print('This is not idented, so it is outside the if block')
 print('This is a second line that is not indented.')
+
+#Define the account ID
+accountID = '12345'
+#Define the project name
+projectName = 'myS3Bucket'
+#Concatenate the strings to form a unique name
+bucketID = accountID + '-' + projectName + '-Bucket'
+#Print the resulting name
+print(f'The bucket name is {bucketID}')

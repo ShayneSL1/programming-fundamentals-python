73ad82087daab4b68e878dd161edace73c366ade
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-03 23:55:06 -0500

    corrected some spelling errors

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ if True:
     print('This is indented')
     print('this is also in the if block')
     print('this is a third line that is indented')
-print('This is not idented, so it is outside the if block')
+print('This is not indented, so it is outside the if block')
 print('This is a second line that is not indented.')
 
 #Define the account ID
@@ -14,6 +14,6 @@ accountID = '12345'
 #Define the project name
 projectName = 'myS3Bucket'
 #Concatenate the strings to form a unique name
-bucketID = accountID + '-' + projectName + '-Bucket'
+bucketID = accountID + '-' + projectName + '-bucket'
 #Print the resulting name
 print(f'The bucket name is {bucketID}')

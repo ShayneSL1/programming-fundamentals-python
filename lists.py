baaa848039076921cf0ab0dd55af995f0d3eefc4
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-06 03:46:31 -0500

    .sort method to sort a list in ascending order

--- lists.py
+++ lists.py
@@ -2,7 +2,7 @@
 instanceIds = ['i-1234','i-5678','i-9012']
 
 #List of IPs for a security group
-ipAddresses = ['10.0.0.1','10.0.0.2','10.0.0.3','10.0.0.4']
+ipAddresses = ['10.0.0.1','10.0.0.2','10.0.0.3','10.0.0.4','10.0.0.0']
 
 #List of AZs in a region
 availabilityZones = ['us-east-1a','us-east-2b','us-east-3c']
@@ -28,5 +28,10 @@ else:
 firstTwoAzs = availabilityZones[:2]
 print("First two AZs:",firstTwoAzs)
 
+#Last availability zone
 lastAZ = availabilityZones[2:]
 print(lastAZ)
+
+#.sort method sorts by ascending order
+ipAddresses.sort()
+print(ipAddresses)

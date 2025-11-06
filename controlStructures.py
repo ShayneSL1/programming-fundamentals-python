ebe03a704e04f74bfd157165e8e5d18e591346c2
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-06 12:34:18 -0500

    Practice with if statement

--- controlStructures.py
+++ controlStructures.py
@@ -1,6 +1,7 @@
 #Control Structures / Control Flow
 
 #If Statements
+instanceRunning = "Running"
 
 """ if condition:
     #code to execute if condition is True
@@ -8,3 +9,10 @@ elif anotherCondition:
     #code to execute if anotherCondition is True
 else:
     #code to execute if all other conditions are False. """
+
+if instanceRunning == "Running":
+    print("The EC2 is running.")
+elif instanceRunning == "Stopped":
+    print("The EC2 is stopped.")
+else:
+    print("The EC2 is in an unexpected state")

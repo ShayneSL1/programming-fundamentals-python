6d8ccf456e1d26c243db60351d97895dbb82302b
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-06 01:54:13 -0500

    Adding to lists in python using the .append function, remove using the .remove function, and checking a list if it includes an item with an if-statement with an "in" operator. if ITEM in LIST: and so on and so on

--- lists.py
+++ lists.py
@@ -2,12 +2,23 @@
 instanceIds = ['i-1234','i-5678','i-9012']
 
 #List of IPs for a security group
-ipAddresses = ['10.0.0.1','10.0.0.2','10.0.0.3']
+ipAddresses = ['10.0.0.1','10.0.0.2','10.0.0.3','10.0.0.4']
 
 #List of AZs in a region
 availabilityZones = ['us-east-1a','us-east-2b','us-east-3c']
 
 #Print the lists
 print(f"EC2 instances to delete: {instanceIds}")
-print(f"IP Addresses: {ipAddresses}")
-print(f"Number of availability zones: {availabilityZones}")
+# print(f"IP Addresses: {ipAddresses}")
+# print(f"Number of availability zones: {availabilityZones}")
+
+#Add new EC2 instance id 
+instanceIds.append('i-3456')
+print(instanceIds)
+
+#Check if list includes an item
+if "10.0.0.4" in ipAddresses:
+    print("Yes 10.0.0.4 is allowed")
+else:
+    print("10.0.0.4 is not in the allowed list")
+

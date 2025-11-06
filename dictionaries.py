64fac5c3de6e89813c49662ae37f6dba6907d3f6
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-06 05:29:50 -0500

    .get function on a dictionary allows you to see whether a key is present in a dictionary. dictionary[thingIWant, message I want printed if thingIWant does not exist]

--- dictionaries.py
+++ dictionaries.py
@@ -7,8 +7,11 @@ ec2Instance = {
     "InstanceId": "i-123abc",
     "InstanceType": "t2-micro",
     "State": "running",
-    "PublicIpAddress": "203.0.111.2"
+    #"PublicIpAddress": "203.0.111.2"
 }
 
 instanceType = ec2Instance["InstanceId"]
 print(instanceType)
+
+publicIp = ec2Instance.get("PublicIpAddress", "No Public IP Address here.")
+print(publicIp)

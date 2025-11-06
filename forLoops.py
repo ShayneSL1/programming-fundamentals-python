8dc10d896dd5008ae3948eaa1394e734e999f654
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-06 12:55:44 -0500

    learning about forLoops

---
+++ forLoops.py
@@ -0,0 +1,12 @@
+#For Loops
+
+""" for item in sequence:
+    #code to execute for each item """
+
+instanceIds = ['i-123','i-456','i-789','i-a123']
+
+for each in instanceIds:
+    print(f"Checking status of instance {each}")
+    print(f"Instance {each} status check complete")
+
+print("All instances have been checked")

40af20f8bfdcb26df81a457a3f0c8a73a5821c45
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-06 12:16:42 -0500

    Learning about if statements

---
+++ controlStructures.py
@@ -0,0 +1,10 @@
+#Control Structures / Control Flow
+
+#If Statements
+
+""" if condition:
+    #code to execute if condition is True
+elif anotherCondition:
+    #code to execute if anotherCondition is True
+else:
+    #code to execute if all other conditions are False. """

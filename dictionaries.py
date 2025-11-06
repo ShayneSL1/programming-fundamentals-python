0d43a593f807a40bedc3031221394ac037dbd6c3
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-06 05:19:00 -0500

    Initial Commit to dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+""" Dictionaries
+Store and retrieve information
+Key and Values """
+
+#EC2
+ec2Instance = {
+    "InstanceId": "i-123abc",
+    "InstanceType": "t2-micro",
+    "State": "running",
+    "PublicIpAddress": "203.0.111.2"
+}

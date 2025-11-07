4afada29dcd55d3f4ca70c04bcbec45bc1aa9fe6
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-06 22:37:23 -0500

    example of calling a function

--- functions.py
+++ functions.py
@@ -1,3 +1,13 @@
+import random
+
 """ def functionName(parameter1, parameter2):
     #function body has code to be executed with inputs/parameters
 return result #optional """
+
+def generateBucketName(projectName):
+    randomSuffix = random.int(1,9999)
+    bucketName = f"{projectName}-bucket-{randomSuffix}"
+    return bucketName
+
+superBucket = generateBucketName("S3Bucket")
+print(superBucket)  
